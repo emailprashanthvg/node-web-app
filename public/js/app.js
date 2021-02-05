c51be82d73e4102302d98d61d9adf0f9fe9f5698
PrashanthVG <emailprashanthvg@gmail.com>
2021-02-05 09:21:59 +0530

    HerokuChanges

--- public/js/app.js
+++ public/js/app.js
@@ -2,31 +2,19 @@
 
 console.log('Client side javascript file is loaded!')
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-
 const locationData = document.querySelector('form')
 const search = document.querySelector('input')
 let messageOne = document.querySelector('#message1')
 let messageTwo = document.querySelector('#message2')
 
-messageOne.textContent = ''
-messageTwo.textContent = ''
-
-
 locationData.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(search.value)
+    // console.log(search.value)
 
     messageOne.textContent = 'Loading'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    fetch('/weather?address=' + search.value).then((response) => {
         // console.log(response)
         response.json().then((data) => {
             if (data.error) {
